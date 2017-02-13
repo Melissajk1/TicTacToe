6c3ff1edb3e745364c420829023859d68b0f9306
Melissajk1 <mely_raygoza@hotmail.com>
2017-02-12 23:30:23 -0700

    Add files via upload

---
+++ TicTacToe/src/tictactoe/TicTacToe.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tictactoe;
+
+/**
+ *
+ * @author Gabriel
+ */
+public class TicTacToe {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JuegoGato g =new JuegoGato();
+        g.setVisible(true);
+        // TODO code application logic here
+    }
+    
+}
